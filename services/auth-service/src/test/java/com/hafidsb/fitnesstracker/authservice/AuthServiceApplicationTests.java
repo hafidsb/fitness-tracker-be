5ee2151c54abf718e79575a48ad5e79ed12b3260
hafidsb <hfdbch@gmail.com>
2026-01-14 22:16:56 +0700

    feat: initiate spring boot + dockerized auth service and db

---
+++ services/auth-service/src/test/java/com/hafidsb/fitnesstracker/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafidsb.fitnesstracker.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
