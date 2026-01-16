0914d95ae7b21dd3cbbc221c79ad1f42379c6969
hafidsb <hfdbch@gmail.com>
2026-01-16 15:19:28 +0700

    feat(user-service): initiate user service

---
+++ services/user-service/src/test/java/com/hafidsb/fitnesstracker/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafidsb.fitnesstracker.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
